9f8ea6116c9d87b0cd088a169c0d103814d2d32a
MehreenT <mehreentahir18@gmail.com>
2016-09-29 13:00:26 +0500

    Channels: Removed commented auto generated code

--- app/channels/chatrooms_channel.rb
+++ app/channels/chatrooms_channel.rb
@@ -1,10 +1,7 @@
-# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class ChatroomsChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
   end
 end
