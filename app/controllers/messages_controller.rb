1b15ca75400d6215293c3fe0288eb71a1af7aba4
MehreenT <mehreentahir18@gmail.com>
2016-09-29 11:04:07 +0500

    Message: Channel speicific message creation functionality intgerated into system

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,24 @@
+class MessagesController < ApplicationController
+  before_action :set_chatroom, only: [:create]
+
+  def create
+    message = @chatroom.messages.new(message_params)
+    message.user = current_user
+    respond_to do |format|
+      if message.save
+        format.html { redirect_to @chatroom, notice: 'Message added to chat.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+    def set_chatroom
+      @chatroom = Chatroom.find(params[:message][:chatroom_id])
+    end
+
+    def message_params
+      params.require(:message).permit(:body)
+    end
+end
