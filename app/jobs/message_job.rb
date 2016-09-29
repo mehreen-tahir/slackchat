09dccf4a08ade3a1018d3b65ae562a31da67d498
MehreenT <mehreentahir18@gmail.com>
2016-09-29 21:18:41 +0500

    Message: Job added & triggered for broadcasting messages over chatrooms

---
+++ app/jobs/message_job.rb
@@ -0,0 +1,11 @@
+class MessageJob < ApplicationJob
+  queue_as :default
+
+  def perform(message)
+    chatroom_id = message.chatroom.id
+     ActionCable.server.broadcast "chatroom_#{chatroom_id}", {
+      message: MessagesController.render(message),
+      chatroom_id: chatroom_id
+    }
+  end
+end
