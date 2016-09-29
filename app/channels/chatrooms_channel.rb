5d81dfcd9c52f8b103ac12f952987d893b429887
MehreenT <mehreentahir18@gmail.com>
2016-09-29 14:49:48 +0500

    Chatroomchannel established for subscribing & unsubscribed

--- app/channels/chatrooms_channel.rb
+++ app/channels/chatrooms_channel.rb
@@ -1,7 +1,11 @@
 class ChatroomsChannel < ApplicationCable::Channel
   def subscribed
+    current_user.chatrooms.each do |chatroom|
+      stream_from "chatroom_#{chatroom.id}"
+    end
   end
 
   def unsubscribed
+    stop_all_streams
   end
 end
